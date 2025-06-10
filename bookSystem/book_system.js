388f5a195fa048090ff077860a7ca600b9de08c0
stg077 <stg07colmenares@gmail.com>
2025-06-09 21:58:15 -0400

    fucnion de elminar

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Nombre del autor:</strong> ${book.authorName}</p>
         <p><strong>Descripción del libro:</strong> ${book.bookDescription}</p>
         <p><strong>Número de páginas:</strong> ${book.pagesNumber} página(s)</p>
-        <button onclick="editbook(${index})">Editar</button>`
+        <button onclick="editbook(${index})">Editar</button>
+        <button onclick="deletebook(${index})">Eliminar</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -44,3 +45,8 @@ function editbook(index) {
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+ function deletebook(index){
+    books.splice(index, 1); // Eliminar entrada antigua
+    showbooks(); // Actualizar lista
+  }
